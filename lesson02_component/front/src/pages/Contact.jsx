aa0502f68b96908bd68959b872c42cca36d3a8c6
oleksandr <2356931k@gmail.com>
2023-02-17 20:34:40 +0200

    commit

---
+++ lesson02_component/front/src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+import {Link} from "react-router-dom";
+
+export default function Contact() {
+    return (
+        <section className='contact'>
+            <h2>Contact</h2>
+            <Link to="/">back Home</Link>
+        </section>
+    )
+}
