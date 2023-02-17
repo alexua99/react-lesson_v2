92397c027bff0beea9f2c866e1bec24bf670ef4c
oleksandr <2356931k@gmail.com>
2023-02-17 21:00:32 +0200

    commitdfg

--- lesson02_component/front/src/pages/Contact.jsx
+++ lesson02_component/front/src/pages/Contact.jsx
@@ -4,7 +4,10 @@ export default function Contact() {
     return (
         <section className='contact'>
             <h2>Contact</h2>
-            <Link to="/">back Home</Link>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/4/4d/Cat_November_2010-1a.jpg" alt="" width='200'/>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem delectus dolore eaque eum laborum maxime molestias nemo veritatis vero voluptates.</p>
+
+             <button>Start</button>
         </section>
     )
 }
